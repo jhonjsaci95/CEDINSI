20e95c5b0f6ebe941c1fcf4521bb81ee5a4bb095
jhonjsaci95 <jhonj_95@live.com>
2024-10-07 21:12:11 -0500

    se cargan los js de las funcionalidades para la pagina y carrito.

--- Modulo-JavaScript/Proyecto-final/Proyecto1/Login/login.js
+++ Modulo-JavaScript/Proyecto-final/Proyecto1/Login/login.js
@@ -8,13 +8,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const username = document.getElementById("username").value;
         const password = document.getElementById("password").value;
 
-        // // Realizar verificación de credenciales aquí (simulado en este ejemplo).
-        // if (username === "manager" && password === "1234567") {
-        //     overlay.style.display = "none"; // Ocultar la superposición modal
-        //     content.style.display = "block"; // Mostrar el contenido protegido
-        // } else {
-        //     alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
-        // }
+        // Realizar verificación de credenciales aquí (simulado en este ejemplo).
+        if (username === "manager" && password === "1234567") {
+            overlay.style.display = "none"; // Ocultar la superposición modal
+            content.style.display = "block"; // Mostrar el contenido protegido
+        } else {
+            alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+        }
     });
 
     // Mostrar la superposición modal inicialmente (bloquea la página)
